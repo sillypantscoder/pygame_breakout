7ba5a179effc6c958941462eab92e50156529b22
sillypantscoder <78504018+sillypantscoder@users.noreply.github.com>
2022-04-11 13:41:34 -0600

    Add numbers to cells

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pygame
 import random
 
+pygame.font.init()
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 BOARDSIZE = [10, 10]
 BOARD = [[random.choices([0, 1], weights=[5, 1], k=1)[0] for x in range(BOARDSIZE[0])] for y in range(BOARDSIZE[1])]
 CELLSIZE = 50
+FONT = pygame.font.SysFont(pygame.font.get_default_font(), round(CELLSIZE / 2))
+FONTHEIGHT = FONT.render("0", True, WHITE).get_height()
 
 screen = pygame.display.set_mode((500, 500))
 
@@ -26,6 +30,8 @@ while running:
 			cellrect = pygame.Rect(x * CELLSIZE, y * CELLSIZE, CELLSIZE, CELLSIZE)
 			if cell > 0:
 				pygame.draw.rect(rendered_board, BLACK, cellrect, 0, 5)
+				number = FONT.render(str(cell), True, WHITE)
+				rendered_board.blit(number, (cellrect.centerx - (number.get_width() / 2), cellrect.centery - (number.get_height() / 2)))
 	screen.blit(rendered_board, (0, 0))
 	# Flip
 	pygame.display.flip()
