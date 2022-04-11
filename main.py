2f77b3764559df1edd0054e7d5d2d874655e5f97
sillypantscoder <78504018+sillypantscoder@users.noreply.github.com>
2022-04-11 13:29:00 -0600

    Bigger & rounded cells

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import random
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-BOARD = [[random.choices([0, 1], weights=[5, 1], k=1)[0] for x in range(10)] for y in range(10)]
-CELLSIZE = 10
+BOARDSIZE = [10, 10]
+BOARD = [[random.choices([0, 1], weights=[5, 1], k=1)[0] for x in range(BOARDSIZE[0])] for y in range(BOARDSIZE[1])]
+CELLSIZE = 50
 
 screen = pygame.display.set_mode((500, 500))
 
@@ -19,12 +20,12 @@ while running:
 	screen.fill(WHITE)
 	rendered_board = pygame.Surface((500, 500))
 	rendered_board.fill(WHITE)
-	for x in range(len(BOARD)):
-		for y in range(len(BOARD[x])):
+	for x in range(BOARDSIZE[0]):
+		for y in range(BOARDSIZE[1]):
 			cell = BOARD[x][y]
 			cellrect = pygame.Rect(x * CELLSIZE, y * CELLSIZE, CELLSIZE, CELLSIZE)
 			if cell > 0:
-				pygame.draw.rect(rendered_board, BLACK, cellrect)
+				pygame.draw.rect(rendered_board, BLACK, cellrect, 0, 5)
 	screen.blit(rendered_board, (0, 0))
 	# Flip
 	pygame.display.flip()
